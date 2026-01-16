62e3fa5c1de9655b949adc2c8c0a61bc8f67e7e2
Bishwas <bishwasmahajan@gmail.com>
2026-01-16 15:34:00 +0545

    feat: Added pop up model in dashboard that shows recent bookings

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MenuPage from './Pages/MenuPage'
 import Testsidebar from './Pages/TestingSidebar'
 import OrderModelPage from './Pages/OrderModelPage'
+import Dashboard from './Features/Dashboard/Dashboard'
 
 function App() {
   return (
     <>
     <BrowserRouter>
     <Routes>
-        <Route path = '/' element={<OrderModelPage/>}/>
+        <Route path = '/orderForm' element={<OrderModelPage/>}/>
         <Route path='/menu' element = {<MenuPage/>}/>
         <Route path = '/sidebar' element={<Testsidebar/>}/>
+        <Route path='/' element={<Dashboard/>}/>
     </Routes>
     </BrowserRouter>
     </>
