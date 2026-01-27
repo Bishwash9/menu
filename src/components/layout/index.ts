286dbba98784e705c9dcfe47453040207a7bc85e
Subarna Ghimire <Subarnaghimirecsit@sanninternational.edu.np>
2026-01-27 13:35:43 +0545

    Delete src/components directory

--- src/components/layout/index.ts
+++
@@ -1,4 +0,0 @@
-export { Header } from './Header';
-export { SideBar } from './Sidebar';
-export { default as DashboardHeader } from './DashboardHeader';
-export {DashboardLayout} from './DashboardLayout';
