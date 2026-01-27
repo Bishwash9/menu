908794d6a6544d1e5ea33c69ac02e9dd3784cf8b
Subarna Ghimire <Subarnaghimirecsit@sanninternational.edu.np>
2026-01-27 13:36:10 +0545

    Delete src/lib directory

--- src/lib/orderdata.ts
+++
@@ -1,6 +0,0 @@
-import type { OrderType } from '../Types/order';
-
-export const MOCK_ORDER_DATA: Record<OrderType, string> = {
-    table: "Table-01",
-    room: "Room-101"
-};
--- src/lib/roles.ts
+++
@@ -1,9 +0,0 @@
-export type Role = 'user' | 'admin' | 'reception' | 'housekeeping' ; 
-
-export const ROLES = {
-    USER : 'user' as Role,
-    ADMIN : 'admin' as Role,
-    RECEPTION : 'reception' as Role,
-    HOUSEKEEPING : 'housekeeping' as Role,
-    COMPANY: 'company' as Role
-};
