f01f1ab7d668fba6c350849b5cd34fe08e9d7396
Bishwas <bishwasmahajan@gmail.com>
2026-02-11 17:54:08 +0545

    Created base API layer

---
+++ src/Config/api.ts
@@ -0,0 +1,20 @@
+const BASE_URL = '';
+
+const apiClient = async(endpoint: string, options: RequestInit = {}) =>{
+    const token = localStorage.getItem('accessToken');
+
+    const headers = {
+        'Content-type' : 'application/json',
+        ...(token && { 'Authorization' : `Bearer ${token}`}),
+        ...options.headers,
+    };
+
+    const response = await fetch(`${BASE_URL}${endpoint}`,{...options, headers});
+
+    if(!response.ok){
+        const error = await response.json();
+        throw new Error(error.message || 'API request failed');
+    } 
+
+    return response.json();
+}   
---
+++ src/Services/authService.ts
@@ -0,0 +1,11 @@
+// import type { LoginResponse } from "../Types/auth";
+
+
+// export const authService ={
+//     login: async(phone:string, password:string):
+//     Promise<LoginResponse> => {
+//         const data = await fetch('',{
+//             method
+//         })
+//     }
+// }
