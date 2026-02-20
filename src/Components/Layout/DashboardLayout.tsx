38e7715979524727ba98859efcf85c07f565148e
Bishwas <bishwasmahajan@gmail.com>
2026-02-20 16:23:15 +0545

    Update: Updated dashboard layout page for dynamically rendering sidebar and header

--- src/Components/Layout/DashboardLayout.tsx
+++ src/Components/Layout/DashboardLayout.tsx
@@ -1,14 +1,19 @@
+import { Outlet } from "react-router-dom"; // Import Outlet
 import DashboardHeader from "./DashboardHeader";
 import { SideBar } from "./Sidebar";
 
-export function DashboardLayout({children}:{children:React.ReactNode}){
-    return(
+export function DashboardLayout() {
+    return (
         <div className="flex h-screen overflow-hidden">
-            <SideBar/>
+            <SideBar />
 
             <div className="bg-gray-50 overflow-y-auto flex-1">
-                <DashboardHeader initials="B"/>
-                {children} {/*esma chai content hunxa (admin,user,staff) which is dynamic with the concept of children*/ }
+                <DashboardHeader initials="B" />
+                
+                {/* Outlet is the dynamic content of page */}
+                <main className="p-6">
+                    <Outlet /> 
+                </main>
             </div>
         </div>
     );
