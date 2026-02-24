e059201247de59cdb4e221cd1ebf630695cebaae
Bishwas <bishwasmahajan@gmail.com>
2026-02-24 14:21:58 +0545

    Updated service layer added type security

--- src/Services/roomService.ts
+++ src/Services/roomService.ts
@@ -1,8 +1,9 @@
 import { apiClient } from "../Config/api";
+import type { Room } from "../Types/room";
 
 export const roomService = {
 
-    getRooms: async (businessId:number) =>{
+    getRooms: async (businessId:number): Promise<Room[]> =>{
         const response = await apiClient(`room/b${businessId}/`, {
             method: 'GET'
         });
@@ -14,8 +15,8 @@ export const roomService = {
         return response.data || response;
     },
 
-    getRoomById : async (businessId:number, roomId:number) =>{
-        const response = await apiClient(`room/${businessId}/${roomId}/`, {
+    getRoomById : async (businessId:number, roomId:number): Promise<Room> =>{
+        const response = await apiClient(`room/b${businessId}/${roomId}/`, {
             method: 'GET'
         });
 
