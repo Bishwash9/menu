23e5e718cde960bcf0646e3ae196aff97a359bca
Bishwas <bishwasmahajan@gmail.com>
2026-02-25 17:11:00 +0545

    Updated dummy page

--- src/Pages/Dummy.tsx
+++ src/Pages/Dummy.tsx
@@ -4,7 +4,7 @@ const Dummy: React.FC = () => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-slate-50">
             <h1 className="text-6xl font-extrabold text-[#002366]">
-                खुस भैर होला
+                खुसी भैरा होला
             </h1>
         </div>
     );
